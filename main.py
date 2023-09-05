ae2d1207c8241bff80862d9bcb635a4d8fd5bf4e
GalinaRbbkv <143825109+GalinaRbbkv@users.noreply.github.com>
2023-09-05 22:39:45 +0700

    Add files via upload
    внизу мои замечания, которые я не могу решить

--- main.py
+++ main.py
@@ -9,50 +9,49 @@ vin = [[1, 2, 3], [1, 3, 2], [2, 1, 3], [2, 3, 1], [3, 2, 1], [3, 1, 2],
        [2, 5, 8], [2, 8, 5], [5, 2, 8], [5, 8, 2], [8, 5, 2], [8, 2, 5],
        [3, 6, 9], [3, 9, 6], [6, 3, 9], [6, 9, 3], [9, 6, 3], [9, 3, 6]]
 def step_x():
-    x = int(input("игрок- Х, сделай ход "))
-    all_steps_x.append(x)
+    x = int(input("игрок- Х, сделай ход - "))
     if x not in list(board):
-        print("неверный ход- выбери от 1 до 9")
+        print(x,"-это не подходит- выбери число из", board)
         step_x()
-        if x in list(all_steps_x) or x in list(all_steps_zero):
-            print("ячейка занята - выбери другую")
-            step_x()
-    index_to_replace = x - 1
-    new_value = "X"
-    board[index_to_replace] = new_value
+    index_to_replace_x = x - 1
+    new_value_x = "X"
+    board[index_to_replace_x] = new_value_x
+    all_steps_x.append(x)
     print(board)
+    print("все ходы игрока Х - ", all_steps_x, "и все ходы игрока О - ", all_steps_zero)
 
 def step_z():
-    zero = int(input("игрок- O, сделай ход "))
-    all_steps_zero.append(zero)
+    zero = int(input("игрок- O, сделай ход - "))
     if zero not in list(board):
-        print("неверный ход- выбери от 1 до 9")
+        print(zero,"-это не подходит- выбери число из", board)
         step_z()
-        if zero not in list(all_steps_x) or zero not in list(all_steps_zero):
-            print("ячейка занята - выбери другую")
-            step_z()
-    index_to_replace = zero - 1
-    new_value = "O"
-    board[index_to_replace] = new_value
+    index_to_replace_z = zero - 1
+    new_value_z = "O"
+    board[index_to_replace_z] = new_value_z
+    all_steps_zero.append(zero)
     print(board)
-print("все ходы игрока Х - ", all_steps_x, "и все ходы игрока О - ", all_steps_zero)
+    print("все ходы игрока Х - ", all_steps_x, "и все ходы игрока О - ", all_steps_zero)
 
 step_x()
 while True:
     if all_steps_x in vin:
-        print("победил игрок Х")
+        print("КОНЕЦ ИГРЫ:  победил игрок Х")
         break
     else:
         step_z()
         if all_steps_zero in vin:
-            print("победил игрок O")
+            print("КОНЕЦ ИГРЫ: победил игрок O")
             break
         else:
             step_x()
-    print(all_steps_x, all_steps_zero)
-    print(*board)
-#  не могу остановить игру при ничьей
+    if len(all_steps_x) + (len(all_steps_x)) >= 9:
+        print("Игра окончена - НИЧЬЯ!!!")
+        break
+
+
 #  работает только пока правильно вставлять цифры и не повторяться
+# не срабатвает если выигрышная комбиная не точна, напрмер не 789 а 1789
+# список всех ходов сохрянает ошибочные ходы
 
 #215386 - победа Х
 #123568 - победа O
