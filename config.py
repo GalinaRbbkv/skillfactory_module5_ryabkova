17a103f72bbdf29d0282acf040e8ae79bb145075
GalinaRbbkv <143825109+GalinaRbbkv@users.noreply.github.com>
2023-10-12 01:25:06 +0700

    Add files via upload
    телеграм бот

---
+++ config.py
@@ -0,0 +1,10 @@
+TOKEN = "6557443844:AAFftGkBt-IVLOkKUuPT9w9a8Rrx0Q9bNFQ"
+
+keys = {
+    "евро": "EUR",
+    "рубль": "RUB",
+    "доллар": "USD",
+    "Евро": "EUR",
+    "Рубль": "RUB",
+    "Доллар": "USD"
+}
